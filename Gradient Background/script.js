0f843a9cc03a7488677a3370b03aefe85788711f
charoufnassro <charoufnassro@gmail.com>
2019-06-19 15:44:18 +0100

    Gradient Background

---
+++ Gradient Background/script.js
@@ -0,0 +1,15 @@
+var color1 = document.getElementById('color1');
+var color2 = document.getElementById('color2');
+var body = document.querySelector('body');
+
+var css = document.querySelector('h3');
+
+function getColor(){
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";";
+}
+body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+css.textContent = body.style.background + ";";
+color1.addEventListener('input',getColor);
+
+color2.addEventListener('input',getColor);
